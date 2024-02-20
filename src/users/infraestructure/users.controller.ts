f3e98df4e5a1dbc61d1b94787a0c60445bbeb390
FernandoEmilianoFloresDeLaRiva <emilianoflores07081@gmail.com>
2024-02-19 20:47:15 -0600

    starting with nest basics...

---
+++ src/users/infraestructure/users.controller.ts
@@ -0,0 +1,48 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
+import { UsersService } from '../application/users.service';
+import { CreateUserDto, UpdateUserDto } from '../domain/dto';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
+  }
+
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  findAll() {
+    return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  @HttpCode(HttpStatus.OK)
+  findOne(@Param('id') id: string) {
+    return this.usersService.findOne(+id);
+  }
+
+  @Patch(':id')
+  @HttpCode(HttpStatus.OK)
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.usersService.update(+id, updateUserDto);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.OK)
+  remove(@Param('id') id: string) {
+    return this.usersService.remove(+id);
+  }
+}
