0039fc25a0899c53e46d7d9de29d67d6aacaaf3b
FernandoEmilianoFloresDeLaRiva <emilianoflores07081@gmail.com>
2024-02-22 23:40:39 -0600

    db connected

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,27 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/infraestructure/users.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
-import { connectionOptions } from './shared/connection/domain/entities/connectionOptions';
 import { TerrariumsModule } from './terrariums/terrariums.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { connectionOptions } from './shared/connection/domain/entities/connectionOptions';
+import { configService } from './shared/config/domain/configEnv';
+
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: `.${process.env.NODE_ENV}.env`,
       isGlobal: true,
     }),
-    TypeOrmModule.forRoot({ ...connectionOptions }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: configService.get('HOST'),
+      port: configService.get('PORT'),
+      username: configService.get('USER'),
+      password: configService.get('PASSWORD'),
+      database: configService.get('DATABASE'),
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
     UsersModule,
     TerrariumsModule,
   ],
