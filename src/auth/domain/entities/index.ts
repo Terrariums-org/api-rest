a96b27a902641a2d5277b6c2b306cf51afa6d43a
Cynthia Vázquez <223205@ids.upchiapas.edu.mx>
2024-03-19 19:46:34 -0600

    token entities

---
+++ src/auth/domain/entities/index.ts
@@ -0,0 +1,5 @@
+export * from './decodeToken.interface';
+export * from './decodeTokenRes.interface';
+export * from './login.interface';
+export * from './payload.interface';
+export * from './tokenRespose.interface';
