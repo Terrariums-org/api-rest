365d322ceac6c6a205814c2d5b3e7472b2261fd4
Cynthia Vázquez <223205@ids.upchiapas.edu.mx>
2024-03-19 19:37:15 -0600

    hashed pasword service done

--- src/auth/infraestructure/auth.module.ts
+++ src/auth/infraestructure/auth.module.ts
@@ -5,10 +5,19 @@ import { UsersModule } from 'src/users/infraestructure/users.module';
 import { UsersService } from 'src/users/application/users.service';
 import { TokenService } from '../aplication/services/token.service';
 import { TokenRepositoryImp } from './ports/TokenRepositoryImp.port';
+import { HashedPasswordService } from '../aplication/services/hashedPassword.service';
+import { BcryptRepositoryImp } from './ports/BcryptRepositoryImp.port';
 
 @Module({
   imports: [UsersModule],
   controllers: [AuthController],
-  providers: [AuthService, UsersService, TokenService, TokenRepositoryImp],
+  providers: [
+    AuthService,
+    UsersService,
+    TokenService,
+    TokenRepositoryImp,
+    HashedPasswordService,
+    BcryptRepositoryImp,
+  ],
 })
 export class AuthModule {}
